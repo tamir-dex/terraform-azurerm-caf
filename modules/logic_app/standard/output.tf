e9db114ce0458b0c416057aee0c4c2f7a6540cb7
joseph@direct-ex.co.il <joseph@direct-ex.co.il>
2024-06-23 18:04:26 +0300

    Remove outputs

--- modules/logic_app/standard/output.tf
+++ modules/logic_app/standard/output.tf
@@ -12,12 +12,3 @@ output "rbac_id" {
   value       = try(azurerm_logic_app_standard.logic_app_standard.identity.0.principal_id, null)
   description = "The Principal ID of the App Service."
 }
-
-output "vnet_integration_value" {
-  value = var.settings.vnet_intergration
-}
-
-output "vnet_integration" {
-  value       = azurerm_logic_app_standard.logic_app_standard.virtual_network_subnet_id
-  description = "The Subet ID to attach the logic app"
-}
