f078f3ba8dcc4ea9de8d1d711ab3b485acac8bcc
joseph@direct-ex.co.il <joseph@direct-ex.co.il>
2024-08-05 16:42:14 +0300

    Update Logic app to PR#1978

--- modules/logic_app/standard/module.tf
+++ modules/logic_app/standard/module.tf
@@ -17,6 +17,9 @@ resource "azurerm_logic_app_standard" "logic_app_standard" {
   storage_account_access_key = local.storage_account.primary_access_key
   https_only                 = lookup(var.settings, "https_only", null)
   app_settings               = local.app_settings
+  version                    = lookup(var.settings, "version", null)
+  virtual_network_subnet_id  = lookup(var.settings, "vnet_integration", null) != null ? can(var.settings.vnet_integration.subnet_id) ? var.settings.vnet_integration.subnet_id : try(var.vnets[try(var.settings.vnet_integration.lz_key, var.client_config.landingzone_key)][var.settings.vnet_integration.vnet_key].subnets[var.settings.vnet_integration.subnet_key].id, try(var.virtual_subnets[var.client_config.landingzone_key][var.settings.vnet_integration.subnet_key].id, var.virtual_subnets[var.settings.vnet_integration.lz_key][var.settings.vnet_integration.subnet_key].id)) : null
+
 
   dynamic "site_config" {
     for_each = lookup(var.settings, "site_config", {}) != {} ? [1] : []
@@ -40,6 +43,14 @@ resource "azurerm_logic_app_standard" "logic_app_standard" {
           support_credentials = lookup(var.settings.site_config.cors, "support_credentials", null)
         }
       }
+    }
+  }
+
+  dynamic "identity" {
+    for_each = lookup(var.settings, "identity", {}) != {} ? [1] : []
+    content {
+      type         = lookup(var.settings.identity, "type", null)
+      identity_ids = can(var.settings.identity.ids) ? var.settings.identity.ids : can(var.settings.identity.key) ? [var.managed_identities[try(var.settings.identity.lz_key, var.client_config.landingzone_key)][var.settings.identity.key].id] : null
     }
   }
 }
