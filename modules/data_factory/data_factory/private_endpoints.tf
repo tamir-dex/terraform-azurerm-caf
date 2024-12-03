9cf4184da77b6ee7c7ef9cc3364a1a5137b2a48b
tamir-dex <119159129+tamir-dex@users.noreply.github.com>
2024-12-03 13:07:03 +0200

    Update private_endpoints.tf

--- modules/data_factory/data_factory/private_endpoints.tf
+++ modules/data_factory/data_factory/private_endpoints.tf
@@ -1,11 +1,11 @@
 module "private_endpoint" {
   source   = "../../networking/private_endpoint"
-  for_each = try(var.private_endpoints, {})
+  for_each = var.remote_objects.private_endpoints
 
   resource_id         = azurerm_data_factory.df.id
   name                = each.value.name
   resource_group_name = local.resource_group_name
-  location            = local.location
+  location            = "region1"
   subnet_id           = can(each.value.subnet_id) ? each.value.subnet_id : var.remote_objects.vnets[try(each.value.lz_key, var.client_config.landingzone_key)][each.value.vnet_key].subnets[each.value.subnet_key].id
   settings            = each.value
   global_settings     = var.global_settings
