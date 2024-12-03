881337a5a33c9cadc6e46854216303ccef98d016
tamir-dex <119159129+tamir-dex@users.noreply.github.com>
2024-12-03 14:57:08 +0200

    Update private_endpoints.tf

--- modules/data_factory/data_factory/private_endpoints.tf
+++ modules/data_factory/data_factory/private_endpoints.tf
@@ -1,16 +1,17 @@
+
 module "private_endpoint" {
   source   = "../../networking/private_endpoint"
-  for_each = var.remote_objects.private_endpoints
+  for_each = lookup(var.settings, "private_endpoints", {})
 
   resource_id         = azurerm_data_factory.df.id
   name                = each.value.name
+  location            = local.location
   resource_group_name = local.resource_group_name
-  location            = var.location
-  subnet_id           = can(each.value.subnet_id) ? each.value.subnet_id : var.remote_objects.vnets[try(each.value.lz_key, var.client_config.landingzone_key)][each.value.vnet_key].subnets[each.value.subnet_key].id
+  subnet_id           = can(each.value.subnet_id) || can(each.value.vnet_key) == false ? try(each.value.subnet_id, var.virtual_subnets[try(each.value.lz_key, var.client_config.landingzone_key)][each.value.subnet_key].id) : var.vnets[try(each.value.lz_key, var.client_config.landingzone_key)][each.value.vnet_key].subnets[each.value.subnet_key].id
   settings            = each.value
   global_settings     = var.global_settings
   tags                = local.tags
   base_tags           = var.base_tags
-  private_dns         = var.remote_objects.private_dns
+  private_dns         = var.private_dns
   client_config       = var.client_config
 }
